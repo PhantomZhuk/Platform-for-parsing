54a4f2010c4dac4bd1e73d5446f3f80cdb6087fd
Maksym Zhuk <bitmaksum550@gmail.com>
2024-12-15 14:43:12 +0200

    add the function of adding services

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,17 @@
-const services = require("../models/services");
-const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
+const { Services, Availability, Search, HTML } = require('../models/services');
 
-module.exports = {};
+module.exports = {
+    createServices: async (req, res) => {
+        try {
+            let { domain, normalText, additionalText, ul, name, link, price, image, pageLink, exists, className } = req.body;
+            const availability = new Availability({ exists, className });
+            const html = new HTML({ ul, name, link, price, image, pageLink, availability });
+            const search = new Search({ normalText, additionalText });
+            const services = new Services({ domain, search, html });
+            await services.save();
+            res.json(services);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
 const controllers = require("../controllers/admin.js");
 module.exports = require("express")
     .Router()
+    .post(`/createServices`, controllers.createServices);
