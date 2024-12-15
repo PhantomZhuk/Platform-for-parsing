c72e8c6b5df0c7ddedd9a9f93b53f8c9dcd470c6
Maksym Zhuk <bitmaksum550@gmail.com>
2024-12-15 15:37:29 +0200

    add functions for deleting and updating services

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,6 @@ const controllers = require("../controllers/admin.js");
 module.exports = require("express")
     .Router()
     .post(`/createServices`, controllers.createServices)
-    .get(`/getServices`, controllers.getServices)
+    .get(`/getServices`, controllers.getServices) 
+    .delete(`/deleteServices/:id`, controllers.deleteServices)
+    .put(`/updateServices/:id`, controllers.updateServices)
