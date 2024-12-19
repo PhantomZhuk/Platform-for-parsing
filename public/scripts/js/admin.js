bf29017f47c419fab0c9de897a7fd8907125e2de
JustCreatedUser <danieldiril490@gmail.com>
2024-12-19 20:25:01 +0200

    improve admin sections transition

--- public/scripts/js/admin.js
+++ public/scripts/js/admin.js
@@ -8,62 +8,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-(() => __awaiter(void 0, void 0, void 0, function* () {
+void (() => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const services = yield (yield fetch("/admin/getServices", { method: "GET" })).json();
-        function fillServicesList(html) {
-            if (services.length == 0)
-                return html.replace(/<ul class="services__list"><\/ul>/g, 
-                /*html*/ `<ul class="services__list"><div class="services__table-empty">No services found</div></div><div class="services__visits">`);
-            function setHTMLPart(info, className, desc) {
-                return /*html*/ `
-                <div class="services__item services__item--${className}">
-                  <span>${desc}</span>
-                  <textarea rows="1" type="text" readonly>${info}</textarea>
-                </div>
-                `;
+        function fillAllSections() {
+            function fillServicesSection() {
+                const servicesListEl = document.querySelector(".services__list");
+                function setHTMLPart(info, className, desc) {
+                    return /*html*/ `
+                  <div class="services__item services__item--${className}">
+                    <span>${desc}</span>
+                    <textarea rows="1" type="text" readonly>${info}</textarea>
+                  </div>
+                  `;
+                }
+                if (services.length == 0)
+                    servicesListEl.innerHTML = /*html*/ `<ul class="services__list"><div class="services__table-empty">No services found</div></div><div class="services__visits">`;
+                else
+                    servicesListEl.innerHTML = services.reduce((prev, service) => {
+                        return (prev +
+                            /*html*/ `
+                <li id="${service.serviceName}">
+                <div class="service-fns"><button class="service-fns__edit">🖉</button><button class="service-fns__delete">🗑</button></div>
+                ${setHTMLPart(service.serviceName, "name", "Service name :")}
+                ${setHTMLPart(service.domain, "domain", "Domain :")}
+                ${setHTMLPart(service.html.name, "html__name", "html name class :")}
+                ${setHTMLPart(service.html.ul, "html__ul", "html ul class :")}
+                ${setHTMLPart(service.html.image, "html__image", "html image class :")}
+                ${setHTMLPart(service.html.pageLink, "html__link", "html pageLink class :")}
+                ${setHTMLPart(service.html.price, "html__price", "html price class :")}
+                ${setHTMLPart(String(service.html.availability.exists), "html__availability-exists", "Shows availability :")}
+                ${setHTMLPart(service.html.availability.className, "html__availability-class", "Availability class :")}
+                ${setHTMLPart(service.search.normalText, "search__normal", "Search normal text :")}
+                ${setHTMLPart(service.search.additionalText, "search__additional", "Search additional text :")}
+                </li>
+                `);
+                    }, "");
             }
-            return html.replace(/<ul class="services__list"><\/ul>/g, 
-            /*html*/ `<ul class="services__list">` +
-                services.reduce((prev, service) => {
-                    return (prev +
-                        /*html*/ `
-              <li id="${service.serviceName}">
-              <div class="service-fns"><button class="service-fns__edit">🖉</button><button class="service-fns__delete">🗑</button></div>
-              ${setHTMLPart(service.serviceName, "name", "Service name :")}
-              ${setHTMLPart(service.domain, "domain", "Domain :")}
-              ${setHTMLPart(service.html.name, "html__name", "html name class :")}
-              ${setHTMLPart(service.html.ul, "html__ul", "html ul class :")}
-              ${setHTMLPart(service.html.image, "html__image", "html image class :")}
-              ${setHTMLPart(service.html.pageLink, "html__link", "html pageLink class :")}
-              ${setHTMLPart(service.html.price, "html__price", "html price class :")}
-              ${setHTMLPart(String(service.html.availability.exists), "html__availability-exists", "Shows availability :")}
-              ${setHTMLPart(service.html.availability.className, "html__availability-class", "Availability class :")}
-              ${setHTMLPart(service.search.normalText, "search__normal", "Search normal text :")}
-              ${setHTMLPart(service.search.additionalText, "search__additional", "Search additional text :")}
-              </li>
-              `);
-                }, "") +
-                "</ul>");
-        }
-        function fillDashboardServices(html) {
-            if (services.length == 0)
-                return html.replace(/<ul class="dashboard__services-list"><\/ul>/g, 
-                /*html*/ `<ul class="services__list"><div class="services__table-empty">No services found</div></div><div class="services__visits">`);
-            return html.replace(/<ul class="dashboard__services-list"><\/ul>/g, `<ul class="dashboard__services-list">` +
-                services.reduce((prev, service) => {
-                    return (prev +
-                        /*html*/ `<li class="dashboard__service">
-              <h4 class="dashboard__service-name">${service.serviceName}</h4>
-              <p class="dashboard__service-increase">+10</p>
-              <div class="dashboard__service-visits">
-                <span>Visitors</span>
-                <span style="font-size: 18px">100</span>
-              </div>
-              <canvas id="dashboard__service-chart-${service.serviceName}"></canvas>
-            </li>`);
-                }, "") +
-                `</ul>`);
+            function fillDashboardSection() {
+                const dashboardEl = document.getElementById("template-dashboard");
+                console.log(dashboardEl);
+                const servicesListEl = dashboardEl.querySelector(".dashboard__services-list");
+                if (services.length == 0)
+                    servicesListEl.innerHTML = /*html*/ `<div class="services__table-empty">No services found</div>`;
+                else
+                    servicesListEl.innerHTML = services.reduce((prev, service) => {
+                        return (prev +
+                            /*html*/ `<li class="dashboard__service">
+                <h4 class="dashboard__service-name">${service.serviceName}</h4>
+                <p class="dashboard__service-increase">+10</p>
+                <div class="dashboard__service-visits">
+                  <span>Visitors</span>
+                  <span style="font-size: 18px">100</span>
+                </div>
+                <canvas id="dashboard__service-chart-${service.serviceName}"></canvas>
+              </li>`);
+                    }, "");
+            }
+            fillDashboardSection();
+            fillServicesSection();
         }
         function switchSection(event, SectionName) {
             const sectionName = (() => {
@@ -73,36 +76,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
                 const label = target.closest("label");
                 if (!label)
                     return null;
-                return label.textContent.trim();
+                return label.textContent.trim().toLowerCase();
             })();
             if (!sectionName)
                 return;
-            const sectionNode = document.querySelector("section");
-            sectionNode.innerHTML = "";
-            let sectionHTML = document.getElementById(`template-${sectionName.toLowerCase()}`).innerHTML;
-            switch (sectionName) {
-                case "Dashboard":
-                    sectionHTML = fillDashboardServices(sectionHTML);
-                    break;
-                case "Users":
-                    break;
-                case "Services":
-                    sectionHTML = fillServicesList(sectionHTML);
-                    break;
-            }
-            sectionNode.insertAdjacentHTML("afterbegin", sectionHTML);
-            Array.from(document.querySelectorAll("[id*=dashboard__service-chart-]")).forEach((canvas) => {
-                console.log(canvas.width);
-                function chart(p) {
-                    p.setup = () => {
-                        p.createCanvas(93, 45, canvas);
-                        p.stroke(122, 158, 68);
-                        p.strokeWeight(3);
-                        p.line(0, 0, p.width / 2, p.height / 2);
-                        p.line(p.width / 2, p.height / 2, p.width, 0);
-                    };
-                }
-                new p5(chart);
+            document
+                .querySelectorAll(`[id*=template-]`)
+                .forEach((template) => {
+                if (template.id === `template-${sectionName}`)
+                    template.removeAttribute("style");
+                else
+                    template.style.display = "none";
             });
         }
         document
@@ -110,7 +94,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
             .addEventListener("click", function (e) {
             switchSection(e, null);
         });
-        switchSection(null, "Dashboard");
+        fillAllSections();
     }
     catch (e) {
         alert(e);
--- public/scripts/ts/admin.ts
+++ public/scripts/ts/admin.ts
@@ -1,4 +1,4 @@
-type Section = "Dashboard" | "Users" | "Services";
+type Section = "dashboard" | "users" | "services";
 declare class p5 {
   constructor(sketch: (p: sketchFns) => void);
   p: sketchFns;
@@ -41,110 +41,112 @@ interface Service {
     additionalText: string;
   };
 }
-(async () => {
+void (async () => {
   try {
     const services: Service[] = await (
       await fetch("/admin/getServices", { method: "GET" })
     ).json();
-
-    function fillServicesList(html: string): string {
-      if (services.length == 0)
-        return html.replace(
-          /<ul class="services__list"><\/ul>/g,
-          /*html*/ `<ul class="services__list"><div class="services__table-empty">No services found</div></div><div class="services__visits">`
-        );
-      function setHTMLPart(info: string, className: string, desc: string) {
-        return /*html*/ `
-                <div class="services__item services__item--${className}">
-                  <span>${desc}</span>
-                  <textarea rows="1" type="text" readonly>${info}</textarea>
-                </div>
-                `;
+    function fillAllSections(): void {
+      function fillServicesSection(): void {
+        const servicesListEl = document.querySelector(".services__list")!;
+        function setHTMLPart(info: string, className: string, desc: string) {
+          return /*html*/ `
+                  <div class="services__item services__item--${className}">
+                    <span>${desc}</span>
+                    <textarea rows="1" type="text" readonly>${info}</textarea>
+                  </div>
+                  `;
+        }
+        if (services.length == 0)
+          servicesListEl.innerHTML = /*html*/ `<ul class="services__list"><div class="services__table-empty">No services found</div></div><div class="services__visits">`;
+        else
+          servicesListEl.innerHTML = services.reduce(
+            (prev: string, service: Service): string => {
+              return (
+                prev +
+                /*html*/ `
+                <li id="${service.serviceName}">
+                <div class="service-fns"><button class="service-fns__edit">🖉</button><button class="service-fns__delete">🗑</button></div>
+                ${setHTMLPart(service.serviceName, "name", "Service name :")}
+                ${setHTMLPart(service.domain, "domain", "Domain :")}
+                ${setHTMLPart(
+                  service.html.name,
+                  "html__name",
+                  "html name class :"
+                )}
+                ${setHTMLPart(service.html.ul, "html__ul", "html ul class :")}
+                ${setHTMLPart(
+                  service.html.image,
+                  "html__image",
+                  "html image class :"
+                )}
+                ${setHTMLPart(
+                  service.html.pageLink,
+                  "html__link",
+                  "html pageLink class :"
+                )}
+                ${setHTMLPart(
+                  service.html.price,
+                  "html__price",
+                  "html price class :"
+                )}
+                ${setHTMLPart(
+                  String(service.html.availability.exists),
+                  "html__availability-exists",
+                  "Shows availability :"
+                )}
+                ${setHTMLPart(
+                  service.html.availability.className,
+                  "html__availability-class",
+                  "Availability class :"
+                )}
+                ${setHTMLPart(
+                  service.search.normalText,
+                  "search__normal",
+                  "Search normal text :"
+                )}
+                ${setHTMLPart(
+                  service.search.additionalText,
+                  "search__additional",
+                  "Search additional text :"
+                )}
+                </li>
+                `
+              );
+            },
+            ""
+          );
       }
-      return html.replace(
-        /<ul class="services__list"><\/ul>/g,
-        /*html*/ `<ul class="services__list">` +
-          services.reduce((prev: string, service: Service): string => {
-            return (
-              prev +
-              /*html*/ `
-              <li id="${service.serviceName}">
-              <div class="service-fns"><button class="service-fns__edit">🖉</button><button class="service-fns__delete">🗑</button></div>
-              ${setHTMLPart(service.serviceName, "name", "Service name :")}
-              ${setHTMLPart(service.domain, "domain", "Domain :")}
-              ${setHTMLPart(
-                service.html.name,
-                "html__name",
-                "html name class :"
-              )}
-              ${setHTMLPart(service.html.ul, "html__ul", "html ul class :")}
-              ${setHTMLPart(
-                service.html.image,
-                "html__image",
-                "html image class :"
-              )}
-              ${setHTMLPart(
-                service.html.pageLink,
-                "html__link",
-                "html pageLink class :"
-              )}
-              ${setHTMLPart(
-                service.html.price,
-                "html__price",
-                "html price class :"
-              )}
-              ${setHTMLPart(
-                String(service.html.availability.exists),
-                "html__availability-exists",
-                "Shows availability :"
-              )}
-              ${setHTMLPart(
-                service.html.availability.className,
-                "html__availability-class",
-                "Availability class :"
-              )}
-              ${setHTMLPart(
-                service.search.normalText,
-                "search__normal",
-                "Search normal text :"
-              )}
-              ${setHTMLPart(
-                service.search.additionalText,
-                "search__additional",
-                "Search additional text :"
-              )}
-              </li>
-              `
-            );
-          }, "") +
-          "</ul>"
-      );
-    }
-    function fillDashboardServices(html: string): string {
-      if (services.length == 0)
-        return html.replace(
-          /<ul class="dashboard__services-list"><\/ul>/g,
-          /*html*/ `<ul class="services__list"><div class="services__table-empty">No services found</div></div><div class="services__visits">`
-        );
-      return html.replace(
-        /<ul class="dashboard__services-list"><\/ul>/g,
-        `<ul class="dashboard__services-list">` +
-          services.reduce((prev: string, service: Service): string => {
-            return (
-              prev +
-              /*html*/ `<li class="dashboard__service">
-              <h4 class="dashboard__service-name">${service.serviceName}</h4>
-              <p class="dashboard__service-increase">+10</p>
-              <div class="dashboard__service-visits">
-                <span>Visitors</span>
-                <span style="font-size: 18px">100</span>
-              </div>
-              <canvas id="dashboard__service-chart-${service.serviceName}"></canvas>
-            </li>`
-            );
-          }, "") +
-          `</ul>`
-      );
+      function fillDashboardSection(): void {
+        const dashboardEl = document.getElementById("template-dashboard")!;
+        console.log(dashboardEl);
+        const servicesListEl = dashboardEl.querySelector(
+          ".dashboard__services-list"
+        )!;
+
+        if (services.length == 0)
+          servicesListEl.innerHTML = /*html*/ `<div class="services__table-empty">No services found</div>`;
+        else
+          servicesListEl.innerHTML = services.reduce(
+            (prev: string, service: Service): string => {
+              return (
+                prev +
+                /*html*/ `<li class="dashboard__service">
+                <h4 class="dashboard__service-name">${service.serviceName}</h4>
+                <p class="dashboard__service-increase">+10</p>
+                <div class="dashboard__service-visits">
+                  <span>Visitors</span>
+                  <span style="font-size: 18px">100</span>
+                </div>
+                <canvas id="dashboard__service-chart-${service.serviceName}"></canvas>
+              </li>`
+              );
+            },
+            ""
+          );
+      }
+      fillDashboardSection();
+      fillServicesSection();
     }
     function switchSection(event: MouseEvent, SectionName: null): void;
     function switchSection(event: null, SectionName: Section): void;
@@ -157,53 +159,23 @@ interface Service {
         const target: HTMLLabelElement = event!.target as HTMLLabelElement;
         const label = target.closest("label");
         if (!label) return null;
-        return label.textContent!.trim() as Section;
+        return label.textContent!.trim().toLowerCase() as Section;
       })();
       if (!sectionName) return;
-      const sectionNode = document.querySelector("section")!;
-      sectionNode.innerHTML = "";
-      let sectionHTML = (
-        document.getElementById(
-          `template-${sectionName.toLowerCase()}`
-        )! as HTMLTemplateElement
-      ).innerHTML;
-      switch (sectionName) {
-        case "Dashboard":
-          sectionHTML = fillDashboardServices(sectionHTML);
-          break;
-        case "Users":
-          break;
-        case "Services":
-          sectionHTML = fillServicesList(sectionHTML);
-
-          break;
-      }
-      sectionNode.insertAdjacentHTML("afterbegin", sectionHTML);
-      Array.from(
-        document.querySelectorAll<HTMLCanvasElement>(
-          "[id*=dashboard__service-chart-]"
-        )
-      ).forEach((canvas) => {
-        console.log(canvas.width);
-        function chart(p: sketchFns) {
-          p.setup = () => {
-            p.createCanvas(93, 45, canvas);
-            p.stroke(122, 158, 68);
-            p.strokeWeight(3);
-            p.line(0, 0, p.width / 2, p.height / 2);
-            p.line(p.width / 2, p.height / 2, p.width, 0);
-          };
-        }
-        new p5(chart);
-      });
+      document
+        .querySelectorAll<HTMLDivElement>(`[id*=template-]`)
+        .forEach((template) => {
+          if (template.id === `template-${sectionName}`)
+            template.removeAttribute("style");
+          else template.style.display = "none";
+        });
     }
-
     document
       .querySelector<HTMLUListElement>("aside ul")!
       .addEventListener("click", function (e) {
         switchSection(e, null);
       });
-    switchSection(null, "Dashboard");
+    fillAllSections();
   } catch (e) {
     alert(e);
   }
